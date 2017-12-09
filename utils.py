eea0fac95d510d41bf9e91ee6d17441d585db292
shm <soerenhm@phys.au.dk>
2017-12-09 21:29:56 +0100

    Test commit

--- utils.py
+++ utils.py
@@ -11,3 +11,4 @@ def robust_crop(img, page_size, width=700, height=600):
     x1, x2 = int(x0-.5*width), int(x0+.5*width)
     y1, y2 = int(y0-.5*height), int(y0+0.5*height)
     return img[y1:y2, x1:x2]
+
