5b37aed06465a12970c327be2d4cb8ef5c727f3e
thomas <thomaslolkschmidt@gmail.com>
2017-12-09 13:16:03 +0100

    Initial commit of 2048 automation with random moves.

---
+++ random_moves.py
@@ -0,0 +1,15 @@
+import webbrowser
+import pyautogui as pag
+import random
+import time
+
+
+webbrowser.open('https://gabrielecirulli.github.io/2048/', new=1)
+time.sleep(5)
+
+moves = ['left', 'right', 'up', 'down']
+for i in range(100):
+    move = moves[random.randint(0, 3)]
+    pag.press(move)
+    print(move)
+    time.sleep(0.1)
