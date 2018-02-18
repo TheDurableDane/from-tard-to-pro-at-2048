2ac4eefe041f02f1a39450be2fecfc703d227513
thomas <thomaslolkschmidt@gmail.com>
2018-02-18 14:22:15 +0100

    Initial version of take_screenshot() function.

--- src/fkn_pro/image_processing.py
+++ src/fkn_pro/image_processing.py
@@ -1,5 +1,17 @@
+import pyautogui as pag
+from skimage.color import rgb2gray
+import numpy as np
+
+
 def take_screenshot():
-    pass
+    """
+    Take screenshot of the entire screen and return it as a grayscale image in
+    a numpy array. The pixel values are in range [0; 1].
+    """
+    img_orig = np.array(pag.screenshot())
+    img_gray = rgb2gray(img_orig)
+
+    return img_gray
 
 
 def detect_cells():
