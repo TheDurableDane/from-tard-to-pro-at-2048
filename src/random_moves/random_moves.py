79e1f08c70fb0925f68b77192cd1b293a698d6f5
shm <soerenhm@phys.au.dk>
2018-02-22 21:20:44 +0100

    fix for relativ paths on windows

--- src/random_moves/random_moves.py
+++ src/random_moves/random_moves.py
@@ -3,6 +3,11 @@ import pyautogui as pag
 import random
 import time
 
+# fix for linking to relative paths
+import os.path
+here = os.path.dirname(os.path.realpath(__file__))
+abs_path = lambda fname: os.path.join(here, fname)
+
 # test upload
 webbrowser.open('https://gabrielecirulli.github.io/2048/', new=1)
 time.sleep(5)
@@ -21,6 +26,6 @@ for i in range(100):
     print(move)
     time.sleep(0.1)
 
-button_location = pag.locateOnScreen('../fkn_pro/data/new_game_button.png')
+button_location = pag.locateOnScreen(abs_path('../fkn_pro/data/new_game_button.png'))
 x_button, y_button = pag.center(button_location)
 pag.click(x_button, y_button)
