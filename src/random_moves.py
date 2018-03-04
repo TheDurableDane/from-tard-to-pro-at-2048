7bf88981d01a404f274e65516bb26472b983bbe5
thomas <thomaslolkschmidt@gmail.com>
2018-03-04 19:58:41 +0100

    Restructured to reflect the documentation.

--- src/random_moves.py
+++ src/random_moves.py
@@ -1,20 +1,37 @@
 import initialization as init
+from image_processing import game_over
 import pyautogui as pag
 import random
 import time
 
 
-# Start the game
-screen_content = init.initialize_game()
+def make_random_move():
+    moves = ['left', 'right', 'up', 'down']
+    move = moves[random.randint(0, 3)]
+    pag.press(move)
+    time.sleep(0.1)
 
-# Play da damn game
-moves = ['left', 'right', 'up', 'down']
-n_games = 2     # number of games to play
-for game in range(n_games):
-    for i in range(20):
-        move = moves[random.randint(0, 3)]
-        pag.press(move)
-        time.sleep(0.1)
 
-    init.restart_game(screen_content.loc['new game', 'x_center'],
-                      screen_content.loc['new game', 'y_center'])
+def save_results():
+    pass
+
+
+def play_randomly():
+    """
+    This is the main function.
+    """
+    # Start the game
+    screen_content = init.initialize_game()
+
+    # Play da damn game
+    while True:
+        make_random_move()
+
+        if game_over():
+            save_results()
+            init.restart_game(screen_content.loc['new game', 'x_center'],
+                              screen_content.loc['new game', 'y_center'])
+
+
+if __name__ == '__main__':
+    play_randomly()
