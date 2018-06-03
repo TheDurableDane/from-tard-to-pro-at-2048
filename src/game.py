420ea9aac8cc39cb21566bbed4d1fa7df432be69
shm <sorenhm0@gmail.com>
2018-06-03 16:47:51 +0200

    implemented move strategies

--- src/game.py
+++ src/game.py
@@ -4,24 +4,54 @@ import numpy as np
 import argparse
 
 
+# pairs pieces in a list of 4 elements (row or column in board)
+# returns new list with all zeros displaced to the right
+# e.g. [2 0 2 4] -> [4 4 0 0]
+def pair_pieces(lst):
+    pieces = [x for x in lst if x > 0]
+    pieces = pieces + [0] # so that last piece can always be paired
+    m = len(pieces)
+    new_lst = []
+    i = 0
+    while i < m-1:
+        first, second = pieces[i], pieces[i+1]
+        if first == second:
+            new_lst.append(first + second)
+            del pieces[i:i+2]
+            m -= 2
+        else:
+            new_lst.append(first)
+            i += 1
+    new_lst = new_lst + [0]*(4-len(new_lst))
+    return new_lst
+
+
 def move_right(board):
-#    return board
-    pass
+    for i in range(4):
+        lst = pair_pieces(board[i,:][::-1])
+        board[i,:] = lst[::-1]
+    return board
 
 
 def move_left(board):
-#    return board
-    pass
+    for i in range(4):
+        lst = pair_pieces(board[i,:])
+        board[i,:] = lst
+    return board
 
 
 def move_up(board):
-#    return board
-    pass
+    for i in range(4):
+        lst = pair_pieces(board[:,i])
+        board[:,i] = lst
+    return board
 
 
 def move_down(board):
-#    return board
-    pass
+    for i in range(4):
+        lst = pair_pieces(board[:,i][::-1])
+        board[:,i] = lst[::-1]
+    return board
 
 
 def empty_fields(board):
@@ -69,20 +99,28 @@ def test_spawn_piece():
         spawn_piece(board)
     print_board(board)
 
+def test_pair_pieces():
+    lst = [2, 4, 2, 4]
+    print(lst)
+    print(pair_pieces(lst))
+
 
 if __name__ == '__main__':
 #    execute_move()
-    test_spawn_piece()
-    pass
-    parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
-    parser.add_argument('-b','--board',
-                        type=int,
-                        help='4x4 Numpy array containing the board',
-                        required=True)
-    parser.add_argument('-m','--move',
-                        type=str,
-                        help='The move you want to make: left, right, up, or down',
-                        required=True)
-    args = parser.parse_args()
+    # pass
+    # parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
+    # parser.add_argument('-b','--board',
+    #                     type=int,
+    #                     help='4x4 Numpy array containing the board',
+    #                     required=True)
+    # parser.add_argument('-m','--move',
+    #                     type=str,
+    #                     help='The move you want to make: left, right, up, or down',
+    #                     required=True)
+    # args = parser.parse_args()
 
-    execute_move(args.board, args.move)
+    test_pair_pieces()
+
+    # execute_move(args.board, args.move)
+
+    # test_spawn_piece()
