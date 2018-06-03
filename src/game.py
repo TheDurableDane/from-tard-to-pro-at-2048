388f386cef9db7bb76b029853a72a94c04a5fdb8
thomas <thomaslolkschmidt@gmail.com>
2018-06-03 14:58:23 +0200

    Added command line functionality.

--- src/game.py
+++ src/game.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import argparse
+
 
 def move_right(board):
 #    return board
@@ -40,5 +42,15 @@ def execute_move(board, move):
 
 
 if __name__ == '__main__':
-#    execute_move()
-    pass
+    parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
+    parser.add_argument('-b','--board',
+                        type=int,
+                        help='4x4 Numpy array containing the board',
+                        required=True)
+    parser.add_argument('-m','--move',
+                        type=str,
+                        help='The move you want to make: left, right, up, or down',
+                        required=True)
+    args = parser.parse_args()
+
+    execute_move(args.board, args.move)
