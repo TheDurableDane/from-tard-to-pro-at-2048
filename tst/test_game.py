fa279aa6e8b71133bfac011f3b246e70da0d3c5e
shm <sorenhm0@gmail.com>
2018-06-03 17:03:34 +0200

    added tests for pair_pieces

--- tst/test_game.py
+++ tst/test_game.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
-sys.path.insert(0, '../src/')
-from game import spawn_piece
+sys.path.insert(0, 'src/')
+from game import spawn_piece, pair_pieces
 
 
 def test_spawn_piece():
@@ -10,4 +10,18 @@ def test_spawn_piece():
     for i in range(4):
         spawn_piece(board)
 
-    assert(np.sum(board == 0) == 12)
+    assert np.sum(board == 0) == 12
+
+def test_pair_pieces():
+    lsts = [[2, 4, 2, 4],
+            [2, 2, 0, 2],
+            [2, 8, 8, 2],
+            [2, 2, 2, 2]]
+    ress = [[2, 4, 2, 4],
+            [4, 2, 0, 0],
+            [2, 16, 2, 0],
+            [4, 4, 0, 0]]
+    for lst, res in zip(lsts, ress):
+        lst = pair_pieces(lst)
+        assert lst == res
+
