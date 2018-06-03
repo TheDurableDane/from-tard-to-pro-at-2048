9053b46edfd503195878239418f47ff56952f418
shm <sorenhm0@gmail.com>
2018-06-03 20:59:21 +0200

    cleaned up __main__

--- src/game.py
+++ src/game.py
@@ -117,16 +117,15 @@ def input_loop():
 
 
 if __name__ == '__main__':
-    input_loop()
-    # parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
-    # parser.add_argument('-b','--board',
-    #                     type=int,
-    #                     help='4x4 Numpy array containing the board',
-    #                     required=True)
-    # parser.add_argument('-m','--move',
-    #                     type=str,
-    #                     help='The move you want to make: left, right, up, or down',
-    #                     required=True)
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
+    parser.add_argument('-b','--board',
+                        type=int,
+                        help='4x4 Numpy array containing the board',
+                        required=True)
+    parser.add_argument('-m','--move',
+                        type=str,
+                        help='The move you want to make: left, right, up, or down',
+                        required=True)
+    args = parser.parse_args()
 
-    # execute_move(args.board, args.move)
+    execute_move(args.board, args.move)
