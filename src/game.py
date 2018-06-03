b9d8ab9a306d07f5c87f926ae3edefb1752e1c9c
thomas <thomaslolkschmidt@gmail.com>
2018-06-03 14:22:37 +0200

    Added first level of functions.

--- src/game.py
+++ src/game.py
@@ -1,1 +1,44 @@
 # encoding: utf-8
+
+def move_right(board):
+#    return board
+    pass
+
+
+def move_left(board):
+#    return board
+    pass
+
+
+def move_up(board):
+#    return board
+    pass
+
+
+def move_down(board):
+#    return board
+    pass
+    
+
+def spawn_piece(board):
+#    return board
+    pass
+
+
+def execute_move(board, move):
+#    if move == 'right:
+#        board = move_right(board)
+#    elif:
+#        pass
+#    .
+#    .
+#    .
+#    board = spawn_piece(board)
+#    
+#    return board
+    pass
+
+
+if __name__ == '__main__':
+#    execute_move()
+    pass
