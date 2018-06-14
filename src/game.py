8844c586c3a7c0455cf54ddb960849b284bd3aea
thomas <thomaslolkschmidt@gmail.com>
2018-06-14 21:08:24 +0200

    Changed behaviour when running main file to enable playing in terminal.

--- src/game.py
+++ src/game.py
@@ -110,15 +110,4 @@ def input_loop():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Mad execution of 2048 move.')
-    parser.add_argument('-b', '--board',
-                        type=int,
-                        help='4x4 Numpy array containing the board',
-                        required=True)
-    parser.add_argument('-m', '--move',
-                        type=str,
-                        help='The move you want to make: left, right, up, or down',
-                        required=True)
-    args = parser.parse_args()
-
-    execute_move(args.board, args.move)
+    input_loop()
