46c2afa5f18a8601bf6c8ff2a4ef137d94cfafa2
thomas <thomaslolkschmidt@gmail.com>
2018-07-11 17:14:44 +0200

    Added possibility of writing results to file.

--- src/random_moves_nogui.py
+++ src/random_moves_nogui.py
@@ -4,13 +4,23 @@
 from game import *
 from random import randint
 import numpy as np
+import datetime
+import pandas as pd
 
 
-#def save_results:
+def write_results(scores, max_tiles):
+    timestamp = str(datetime.datetime.now()).replace('-', '').replace(':', '').replace(' ', '').replace('.', '')
+    filename = "random_moves_results_{0}.csv".format(timestamp)
+    data = dict(score=scores, max_tile=max_tiles)
+    df = pd.DataFrame(data=data)
+    df.to_csv(filename, index=False)
+
+
 scores = []
 max_tiles = []
 moves = ['l', 'r', 'u', 'd']
 number_of_games = 1000
+write_results_to_file = True
 
 for game_number in range(number_of_games):
     game = Game()
@@ -21,3 +31,6 @@ for game_number in range(number_of_games):
     max_tiles.append(np.max(game.board))
     percentage = round(game_number/number_of_games*100)
     print("{0}% of {1} games completed.".format(percentage, number_of_games), end='\r', flush=True)
+
+if write_results_to_file:
+    write_results(scores, max_tiles)
