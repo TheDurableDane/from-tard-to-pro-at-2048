0a24d81ce6916fb5b22359d2936e135972925985
shm <sorenhm0@gmail.com>
2018-07-11 15:01:53 +0200

    implementation of working keyboard inputs

--- src/game_gui.py
+++ src/game_gui.py
@@ -3,21 +3,25 @@
 from game import *
 import tkinter as tk
 
+_translator = {"Left":"l", "Right":"r", "Up":"u", "Down":"d"}
 
 my_game = Game()
 
-
 root = tk.Tk()
 root.geometry("400x400")
-
-
 
 gametext = tk.Label(root,   text=repr(my_game),
                             font=("Consolas", 20))
 gametext.pack(fill="both", expand=True)
 
 
+def on_key_down(event):
+    key = event.keysym
+    if key in _translator.keys():
+        execute_move(my_game, _translator[key])
+        gametext.config(text = repr(my_game))
 
 
+root.bind("<Key>", on_key_down)
 
 root.mainloop()
