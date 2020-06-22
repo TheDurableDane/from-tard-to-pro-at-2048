9c746c1a2e7fc1ecd28dbad38694da830e504463
Thomas <thomaslolkschmidt@gmail.com>
2020-06-22 21:26:10 +0200

    Moved content to function.

--- src/game_automation/random_moves_nogui.py
+++ src/game_automation/random_moves_nogui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from game import Game, is_game_over, execute_move
+from game import game_control
 from random import randint
 import numpy as np
 import datetime
@@ -9,27 +9,32 @@ import pandas as pd
 
 def write_results(scores, max_tiles):
     timestamp = str(datetime.datetime.now()).replace('-', '').replace(':', '').replace(' ', '').replace('.', '')
-    filename = "results/random_moves_results_{0}.csv".format(timestamp)
+    filename = f'../data/random_moves_results_{timestamp}.csv'
     data = dict(score=scores, max_tile=max_tiles)
     df = pd.DataFrame(data=data)
     df.to_csv(filename, index=False)
 
 
-scores = []
-max_tiles = []
-moves = ['l', 'r', 'u', 'd']
-number_of_games = 1000
-write_results_to_file = True
+def main():
+    scores = []
+    max_tiles = []
+    moves = ['l', 'r', 'u', 'd']
+    number_of_games = 100000
+    write_results_to_file = True
 
-for game_number in range(number_of_games):
-    game = Game()
-    while not is_game_over(game):
-        move = moves[randint(0, len(moves)-1)]
-        execute_move(game, move)
-    scores.append(game.score)
-    max_tiles.append(np.max(game.board))
-    percentage = round(game_number/number_of_games*100)
-    print("{0}% of {1} games completed.".format(percentage, number_of_games), end='\r', flush=True)
+    for game_number in range(number_of_games):
+        game = game_control.Game()
+        while not game_control.is_game_over(game):
+            move = moves[randint(0, len(moves)-1)]
+            game_control.execute_move(game, move)
+        scores.append(game.score)
+        max_tiles.append(np.max(game.board))
+        percentage = round(game_number/number_of_games*100)
+        print("{0}% of {1} games completed.".format(percentage, number_of_games), end='\r', flush=True)
 
-if write_results_to_file:
-    write_results(scores, max_tiles)
+    if write_results_to_file:
+        write_results(scores, max_tiles)
+
+
+if __name__ == '__main__':
+    main()
